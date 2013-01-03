bbd72d0147ad0383c3795ec1541637ad73f313e3
Renug <liang.rannger@gmail.com>
2013-01-04 00:50:22 +0800

    v

--- streams.lisp
+++ streams.lisp
@@ -3,17 +3,24 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defvar *binary-input-stream*
     #+sbcl 'sb-gray:fundamental-binary-input-stream
-    #+ccl 'gray:fundamental-binary-input-stream)
+    #+ccl 'gray:fundamental-binary-input-stream
+    #+allegro 'excl:fundamental-binary-input-stream
+    #+lispworks 'stream:fundamental-binary-input-stream)
   (defvar *binary-output-stream*
     #+sbcl 'sb-gray:fundamental-binary-output-stream
-    #+ccl 'gray:fundamental-binary-output-stream)
+    #+ccl 'gray:fundamental-binary-output-stream
+    #+allegro 'excl:fundamental-binary-output-stream
+    #+lispworks 'stream:fundamental-binary-output-stream)
   (defvar *stream-read-byte-function*
     #+sbcl 'sb-gray:stream-read-byte
-    #+ccl 'gray:stream-write-byte)
+    #+ccl 'gray:stream-read-byte
+    #+allegro 'excl:stream-read-byte
+    #+lispworks 'stream:stream-read-byte)
   (defvar *stream-write-byte-function*
     #+sbcl 'sb-gray:stream-write-byte
-    #+ccl 'gray:stream-write-byte))
-
+    #+ccl 'gray:stream-write-byte
+    #+allegro 'excl:stream-write-byte
+    #+lispworks 'stream:stream-write-byte))
 
 (defclass png-data-input-stream (#.*binary-input-stream*)
   ((data-list :initform (list)
